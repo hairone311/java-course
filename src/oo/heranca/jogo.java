83184442b2e7eed550bd41d933b40ba61a3ac55c
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-06-18 18:10:10 +0200

    Herança #03

--- src/oo/heranca/jogo.java
+++ src/oo/heranca/jogo.java
@@ -8,9 +8,9 @@ public class jogo {
         monstro.x = 10;
         monstro.y = 10;
 
-        Heroi heroi = new Heroi();
-        heroi.x = 10;
-        heroi.y = 11;
+        Heroi heroi = new Heroi(10, 11);
+        // heroi.x = 10;
+        // heroi.y = 11;
 
         System.out.println("Monstro tem => " + monstro.vida + " vidas");
         System.out.println("Heroi tem => " + heroi.vida + " vidas");
