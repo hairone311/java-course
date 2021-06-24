b67af375a6c367eae99d57da47f29a9f058deb38
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-06-24 08:58:11 +0200

    Classe Abstrata #02

---
+++ src/oo/abstrato/Animal.java
@@ -0,0 +1,10 @@
+package oo.abstrato;
+
+public abstract class Animal {
+    
+    public String respirar() {
+        return "Usando Oxigenio!";
+    }
+
+    public abstract String mover();
+}
---
+++ src/oo/abstrato/Cachoro.java
@@ -0,0 +1,14 @@
+package oo.abstrato;
+
+public class Cachoro extends Mamifero {
+
+    @Override
+    public String mamar() {
+        return "Usando leite!";
+    }
+
+    @Override
+    public String mover() {
+        return "Usando as patas!";
+    }
+}
