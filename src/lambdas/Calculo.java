fec19a49310206fc88615691e22bb08cfebe2274
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-06-28 18:57:47 +0200

    Interfaces Funcionais #02

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -4,4 +4,12 @@ package lambdas;
 public interface Calculo {
 
     double executar(double a, double b);
+
+    default String legal() {
+        return "Legal";
+    }
+
+    static String muitoLegal() {
+        return "Muito legal";
+    }
 }
--- src/lambdas/CalculoTeste2.java
+++ src/lambdas/CalculoTeste2.java
@@ -12,5 +12,8 @@ public class CalculoTeste2 {
 
         calc = (x, y) -> x * y;
         System.out.println(calc.executar(2, 3));
+
+        System.out.println(calc.legal());
+        System.out.println(Calculo.muitoLegal());
     }
 }
