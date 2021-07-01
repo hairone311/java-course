4e4506825d2705ee6a348d16ab64a740f4f12518
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-07-01 11:24:16 +0200

    Foreach Funcional #01

---
+++ src/lambdas/Foreach.java
@@ -0,0 +1,23 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+    
+    public static void main(String[] args) {
+        
+        List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+
+        System.out.println("Forma Tradicional...");
+        for(String nome: aprovados) {
+            System.out.println(nome);
+        }
+
+        System.out.println("\nLambda #01...");
+        aprovados.forEach(nome -> System.out.println(nome + "!"));
+
+        System.out.println("\nMethod Reference...");
+        aprovados.forEach(System.out::println);
+    }
+}
