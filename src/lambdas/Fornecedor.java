d7192b24903d71f9ebd72bc1e66facd1a4bc6b9a
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-07-08 14:02:04 +0200

    Supplier

---
+++ src/lambdas/Fornecedor.java
@@ -0,0 +1,16 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+    
+    public static void main(String[] args) {
+        
+        Supplier<List<String>> umaLista = 
+                () -> Arrays.asList("Ana", "Bia", "Gui", "Lia");
+
+        System.out.println(umaLista.get());
+    }
+}
