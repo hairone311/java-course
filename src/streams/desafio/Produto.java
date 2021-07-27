c290c278053a7c2f53710ab0f2f8d8766ceeb326
Eliseu Hairone <eliseuarviscoh@gmail.com>
2021-07-27 16:54:38 +0200

    Desafio Filter #02

---
+++ src/streams/desafio/Produto.java
@@ -0,0 +1,16 @@
+package streams.desafio;
+
+public class Produto {
+
+    final String nome;
+    final double preco;
+    final double desconto;
+    final double frete;
+
+    public Produto(String nome, double preco, double desconto, double frete) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.frete = frete;
+    }
+}
